080e97b3b944eefef405051ec2c7b08db666df25
Davide Giacomelli <giacomelli.davide94@gmail.com>
2014-11-29 01:38:07 +0000

    Update custom_functions.php

--- custom_functions.php
+++ custom_functions.php
@@ -96,7 +96,7 @@ function  user_contactmethods($profile_fields)
 	$age_range = array(
 		array('age' => '18-24', 'value' => '18-24'),
 		array('age' => '25-40', 'value' => '25-40'),
-		array('age' => '>30', 'value' => '>30'),
+		array('age' => '>30', 'value' => '>30')
 	);
 	return $age_range;
 }
