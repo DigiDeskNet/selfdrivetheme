1674e54f35fe929052b95373f3d9dc51991d555c
ilyass94 <ilyass.ennaimi@gmail.com>
2014-11-29 01:54:30 +0000

    Changed bizzVersion

--- custom_functions.php
+++ custom_functions.php
@@ -91,9 +91,8 @@ function modify_contact_methods($profile_fields) {
 
 }
 
-add_filter('user_contactmethods', 'modify_contact_methods')
-function  user_contactmethods($profile_fields)
-{
+add_filter('bizz_car_agerange', 'custom_car_agerange');
+function custom_car_agerange {
 	$age_range = array(
 		array('age' => '18-24', 'value' => '18-24'),
 		array('age' => '25-29', 'value' => '25-29'),
